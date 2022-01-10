738fb101f1a7722754ea828dd9b48b0ad5604ce0
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2022-01-10 06:40:17 +0200

    Feat start work on a monthly subscription plan
    create product plan on stripe
    add columns is_recurring, plan_id for widgets
    modify widget model to the new added columns
    create stubs for the front end page and handler
    work on front end form, then add the js, and the back end logic in order to allow users to buy a subscription

--- internal/models/models.go
+++ internal/models/models.go
@@ -37,6 +37,8 @@ type Widget struct {
 	InventoryLevel int       `json:"inventory_level"`
 	Price          int       `json:"price"`
 	Image          string    `json:"image"`
+	IsRecurring    bool      `json:"is_recurring"`
+	PlanID         string    `json:"plan_id"`
 	CreatedAt      time.Time `json:"-"`
 	UpdatedAt      time.Time `json:"-"`
 }
@@ -123,6 +125,7 @@ func (m *DBModel) GetWidget(id int) (Widget, error) {
 	row := m.DB.QueryRowContext(ctx, `
 		select 
 			id, name, description, inventory_level, price, coalesce(image, ''),
+		       is_recurring, plan_id,
 			created_at, updated_at
 		from 
 			widgets 
@@ -134,6 +137,8 @@ func (m *DBModel) GetWidget(id int) (Widget, error) {
 		&widget.InventoryLevel,
 		&widget.Price,
 		&widget.Image,
+		&widget.IsRecurring,
+		&widget.PlanID,
 		&widget.CreatedAt,
 		&widget.UpdatedAt,
 	)
