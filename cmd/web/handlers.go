ebd2375dc25629bc5393dbf904bc44702af299bf
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2022-01-18 20:38:20 +0200

    Feat create the url on front-end
    create the url on the front-end to look for reset-password and then validate the signature for that url
    need the url for the front-end and the secret key
    set up the route to the page that display the password reset form page
    call the back-end to change the password: set up a route on the back-end to take care of the actual resetting of the password
    create a db for reset user password
    
    until now
    user able to reset password successfully

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ahmedkhaeld/ecommerce/internal/cards"
 	"github.com/ahmedkhaeld/ecommerce/internal/models"
+	"github.com/ahmedkhaeld/ecommerce/internal/urlsigner"
 	"github.com/go-chi/chi/v5"
 	"net/http"
 	"strconv"
@@ -350,6 +352,28 @@ func (app *application) Logout(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) ForgotPassword(w http.ResponseWriter, r *http.Request) {
 	if err := app.renderTemplate(w, r, "forgot-password", &templateData{}); err != nil {
+		app.errorLog.Print(err)
+	}
+}
+func (app *application) ShowResetPassword(w http.ResponseWriter, r *http.Request) {
+	// validate the url, make sure that url is signed
+	theURL := r.RequestURI
+	testURL := fmt.Sprintf("%s%s", app.config.frontend, theURL)
+
+	signer := urlsigner.Signer{
+		Secret: []byte(app.config.secretkey),
+	}
+	valid := signer.VerifyToken(testURL)
+	if !valid {
+		app.errorLog.Println("invalid url - tampering detected")
+		return
+	}
+	data := make(map[string]interface{})
+	data["email"] = r.URL.Query().Get("email")
+
+	if err := app.renderTemplate(w, r, "reset-password", &templateData{
+		Data: data,
+	}); err != nil {
 		app.errorLog.Print(err)
 	}
 }
