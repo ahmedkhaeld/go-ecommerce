b7794d130aef7dcbc650658958b753a414fc9c55
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2022-01-23 19:47:40 +0200

    Feat add a status column for sales, and improve a sale page
    improve the displaying the sale page, after click on the refund button, it should disappear, show the status for the order
    update the status to refunded in the database

--- internal/models/models.go
+++ internal/models/models.go
@@ -453,7 +453,6 @@ func (m *DBModel) GetAllSubscriptions() ([]*Order, error) {
 	return orders, nil
 }
 
-// GetOrderByID
 func (m *DBModel) GetOrderByID(id int) (Order, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
@@ -508,4 +507,18 @@ func (m *DBModel) GetOrderByID(id int) (Order, error) {
 	}
 
 	return o, nil
+}
+
+func (m *DBModel) UpdateOrderStatus(id, statusID int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := "update orders set status_id = ? where id = ?"
+
+	_, err := m.DB.ExecContext(ctx, stmt, statusID, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
